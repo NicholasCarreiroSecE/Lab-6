eafcc15ef5d4c81491c9c68f5cf6f0aa9b35624b
NicholasCarreiroSecE <n01492047@humbermail.ca>
2024-10-08 08:54:40 -0400

    Update lab6.js
    declared the variables of myNumber and myString giving them their respected values then printed them into the console using console.log

---
+++ lab6.js
@@ -0,0 +1,7 @@
+//--- Task One ---//
+
+let myNumber = 10; //--- Declares myNumber and sets the value to 10 ---//
+let myString = "Hello, JavaScript!";  //--- Declares myString and sets the value to Hello, JavaScript! ---//
+
+console.log(myNumber);
+console.log(myString); //--- Console.log prints both variables into the output ---//
