1e56f1b6b8fee2fb6a8aaaee31516d694cdf2e16
NicholasCarreiroSecE <n01492047@humbermail.ca>
2024-10-08 09:56:07 -0400

    Update lab6.js
    Finished task 2-3

--- lab6.js
+++ lab6.js
@@ -5,3 +5,28 @@ let myString = "Hello, JavaScript!";  //--- Declares myString and sets the value
 
 console.log(myNumber);
 console.log(myString); //--- Console.log prints both variables into the output ---//
+
+//--- Task Two ---//
+
+for (let i = 0; i <= 10; i += 1) { //--- For Loop iterates numbers 0-10 by checking if i is => 10 if not it prints the number and adds one then repeats
+    console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) { //--- For Loop multiplies 3 by loop counter each time from one to five printing thr result ---//
+    let multiple = 3 * i;
+    console.log(multiple);
+}
+
+
+//--- Task Three ---//
+
+function addNumbers(num1, num2) { //--- Function defines the function, returns number one plus number two and calls the funtion with inputs ---//
+    return num1 + num2; 
+}
+console.log(addNumbers(15, 15));
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplyNumbers(15, 15));
+
